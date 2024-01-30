42a4ee822c1e5383f2785aff44f5d6bf1752fbb3
Supriya 007 <singhsupriya711@gmail.com>
2024-01-30 10:34:01 +0530

    second commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,31 +1,115 @@
+import { useState } from 'react';
 import bannerImage from '../assets/pic.jpg';
 import bannerbg from '../assets/shape3.svg';
+import { useEffect } from 'react';
+import { useTypewriter } from 'react-simple-typewriter';
+import { ImageData } from '../constants';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+const settings={
+    dots:false,infinite:true,speed:500,slidesToShow:3,slidesToScroll:1
+}
+
+
 
 const Banner=()=>{
+    const [typeEffect]=useTypewriter({
+        words:["FrontEnd Developer","Content Writer","UI/UX Designer","Learner"],
+        loop:{}
+        
+    })
+    const [theme,setTheme]=useState(
+        localStorage.getItem('theme')? localStorage.getItem("theme"):"desktop"
+    );
+    const element=document.documentElement
+    const darkQuery=window.matchMedia("(prefers-color-scheme:dark)");
+
+    const options=[
+        {
+            icon:"fa fa-sun",
+            text:"light"
+        },{
+            icon:"fa fa-moon",
+            text:"dark"
+        },{
+            icon:"fa fa-desktop",
+            text:"desktop"
+        }
+    ];
+    function onWindowMatch(){
+        if(localStorage){
+            if(localStorage.theme==='dark'||(!("theme" in localStorage ) && darkQuery.matches) ){
+                element.classList.add('dark');
+            }else{
+                element.classList.remove("dark");
+            }
+        }
+    }
+    useEffect(()=>{
+            switch(theme){
+                case 'dark':
+                    element.classList.add('dark');
+                    localStorage.setItem('theme','dark')
+                    break;
+
+                case 'light':
+                    element.classList.remove('dark');
+                    localStorage.setItem("theme",'light')
+                    break;
+               
+                deafult:
+                localStorage.removeItem("theme")
+                break;
+            }
+    },[theme])
     return (
         <>
-            <div className="main w-full h-screen  items-center flex flex-col justify-center" style={{backgroundImage:`url(${bannerbg})`,
-                                backgroundSize:"cover"}}>
-            <div className=' w-full flex justify-center'>{/* image */}
-                <img className=" rounded-full shadow-lg w-64 h-64 object-cover" src={bannerImage}/>
-                </div>
+        
+        <section className='bg-red-300 w-full h-screen flex items-center justify-center'>
+        <div className="main  w-2/3 h-2/3  bg-white rounded-lg  duration-100  items-center flex flex-col justify-center dark:bg-slate-800 dark:text-white" 
+                                >
+                                    <div className="fixed top-0 right-10 duration-100 dark:bg-slate-800 dark:text-white bg-gray-100 rounded">
+                                        
+                                        {
+                                            options?.map(opt=>(
+                                                <button 
+                                                key={opt.text}
+                                                onClick={()=>setTheme(opt.text)} 
+                                                className={`w-8 b-8 leading-9 text-xl rounded-full m-1 ${theme==opt.text && "text-sky-600"}`}>
+                                                <i className={opt.icon}></i>
+                                            </button>
+                                            ))
+                                        }
+                                        
+                                    </div>
+                                    <div className='  flex border justify-center'>{/* image */}
+                                    {/* <Slider {...settings}> */}
+                                    {ImageData.map((d)=>(
+                                        <img className="  shadow-lg w-32 h-32  object-cover" src={d.image}/>
+                                    ))}
+                                    {/* </Slider> */}
+                                    
+                                        </div>
+
+            
                 <div 
-             className=' w-1/3 flex justify-center text-gray-400 '>
+             className=' w-2/3 h-2/3  flex justify-center text-gray-400 dark:bg-slate-100 dark:text-black '>
                     <div className='px-4' >
                     <h3 className='mt-3 text-3xl font-semibold' >Hey! I'm,</h3>
-                <h1 className='mt-3 text-5xl font-bold'>Supriya Singh</h1>
-                <h2 className='mt-3 text-2xl '>I am a Frontend Developer</h2>
+                <h1 className='mt-1 text-3xl font-bold '>Supriya Singh</h1>
+                <h2 className='mt-3 text-2xl '>I am a {typeEffect}</h2>
                 <p className='mt-3'>The things that wakes me up everyday is my passion.Its the fuel for my journey.I'm a learner and always be.
                 Looking forward in future to know more about technology and have myself greatly contributing to it.
                 So Have a great day!
                 Never stop dreaming even if peoples out there laughing.</p>
                 <br/>
-                <div className='icon_container space-x-5 flex '>
-                    <a href="" className="cursor-pointer animate-pulse hover:bg-orange-500 px-3 py-4 w-14 h-14 rounded-full flex justify-center bg-black items-center">
+                <div className='icon_container space-x-5 mb-5 flex '>
+                    <a href="" className="cursor-pointer animate-pulse hover:bg-orange-500 px-3 py-4 w-14 h-14 rounded-full flex justify-center bg-black dark:bg-white items-center">
                     <i className='text-3xl fa-brands fa-github'></i>
 
                     </a>
-                    <a href="" className="cursor-pointer animate-pulse hover:bg-orange-500 px-3 py-4 w-14 h-14 rounded-full flex justify-center bg-black items-center">
+                    <a href="" className="cursor-pointer animate-pulse hover:bg-orange-500 px-3 py-4 w-14 h-14 rounded-full flex justify-center bg-white text-black dark:bg-black dark:text-white items-center">
                     <i className='text-3xl  fa-brands fa-linkedin'></i>
 
                     </a>
@@ -33,15 +117,17 @@ const Banner=()=>{
 
 
                 </div> 
-                <br></br>
+                
 
-                <a className='px-3 py-2 bg-black rounded-full mt-4 shadow-lg' href="/contact">Know More</a>
+                <a className='px-3 py-2 bg-white font-bold  text-2xl text-black rounded-full  shadow-lg dark:bg-black dark:text-white ' href="/contact">Contact me</a>
                     </div>
                     {/* text */}
                 
                 </div>
                 
             </div>
+        </section>
+            
         </>
     )
 }
---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+import img1 from "../assets/1.jpeg";
+import img2 from "../assets/2.jpeg";
+import img3 from "../assets/3.jpeg";
+import img4 from "../assets/4.jpeg";
+
+
+
+export const ImageData=[
+    
+        {text:"first",
+            image:img1
+},
+{text:"second",
+image:img2
+},
+{text:"third",
+image:img3
+},
+{text:"fourth",
+image:img4
+},
+    
+]
